f6afb49bbee2a3801930e17441e5ea1326e884a8
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2024-05-15 12:47:05 +0200

    wait for mono runtime to be disabled on project termination

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/meadow/configurations/MeadowAppProcessHandler.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/meadow/configurations/MeadowAppProcessHandler.kt
@@ -3,14 +3,32 @@ package com.jetbrains.rider.plugins.meadow.configurations
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.rd.util.lifetime
+import com.jetbrains.rd.util.error
+import com.jetbrains.rd.util.getLogger
+import com.jetbrains.rd.util.threading.coroutines.launch
+import com.jetbrains.rdclient.util.idea.pumpMessages
 import com.jetbrains.rider.plugins.meadow.devices.MeadowDevice
 import com.jetbrains.rider.plugins.meadow.model.meadowPluginModel
 import com.jetbrains.rider.projectView.solution
+import kotlinx.coroutines.Dispatchers
 import java.io.OutputStream
+import java.time.Duration
+
+private val logger = getLogger<MeadowAppProcessHandler>()
+
+private val MONO_RUNTIME_DISABLE_TIMEOUT = Duration.ofSeconds(10)
 
 class MeadowAppProcessHandler(private val meadowDevice: MeadowDevice, private val project: Project) : ProcessHandler() {
     override fun destroyProcessImpl() {
-        project.solution.meadowPluginModel.terminate.start(project.lifetime, createRunnerInfoOnPort(meadowDevice))
+        val job = project.lifetime.launch(Dispatchers.Default) {
+            project.solution.meadowPluginModel.terminate.startSuspending(createRunnerInfoOnPort(meadowDevice))
+        }
+
+        pumpMessages(MONO_RUNTIME_DISABLE_TIMEOUT) { job.isCompleted }
+        if (!job.isCompleted) {
+            logger.error { "Mono runtime wasn't disabled in $MONO_RUNTIME_DISABLE_TIMEOUT" }
+        }
+
         notifyProcessTerminated(0)
     }
 
