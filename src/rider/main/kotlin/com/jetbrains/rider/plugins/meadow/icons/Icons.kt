a7d38ef58592001ebaf3a7412ad0f8de786feb4c
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2024-05-15 12:35:20 +0200

    update icons

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/meadow/icons/Icons.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/meadow/icons/Icons.kt
@@ -3,5 +3,5 @@ package com.jetbrains.rider.plugins.meadow.icons
 import com.intellij.openapi.util.IconLoader
 
 object Icons {
-    val Main = IconLoader.getIcon("/icons/icon.png", Icons::class.java)
+    val Main = IconLoader.getIcon("/icons/icon.svg", Icons::class.java)
 }
