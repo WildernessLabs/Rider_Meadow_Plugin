ce16e1b1aab2fc52846e94930fbc118c60b68d71
CartBlanche <savagesoftware@gmail.com>
2026-02-09 19:46:48 +0000

    Fix: update DAP adapter source to use Meadow.Debugging.Host directly instead of VS2022

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ val dotNetPluginId: String by project
 val dotNetSrcDir = File(projectDir, "src/dotnet")
 val dotNetProjectPath = File("$dotNetSrcDir/$dotNetPluginId/$dotNetPluginId.csproj")
 
-val dapAdapterSourceDir = File(projectDir.parentFile, "VS_Win_Meadow_Extension/VS_Meadow_Extension/VS_Meadow_Extension.2022/DapAdapter")
+val dapAdapterSourceDir = File(projectDir.parentFile, "Meadow.Debugging.Host/bin/${buildConfiguration}/net8.0")
 val dapAdapterDestDir = File(projectDir, "src/rider/resources/DapAdapter")
 
 version = pluginVersion
